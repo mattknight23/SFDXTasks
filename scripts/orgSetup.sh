96ccca0ab50cf82f6b5e13b5dd43fc71adb26368
Matt Knight <matt@ekwis.com>
2019-10-03 12:05:10 +0100

    Updated org setup script

--- scripts/orgSetup.sh
+++ scripts/orgSetup.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
 DURATION=7
+ALIAS="MYSCRATCHORG"
+PACKAGE_VERSION_ID=""
 
 if [ "$#" -eq 1 ]; then
   DURATION=$1
 fi
 
-sfdx force:org:create -a LogisenseUtilities -s -f config/project-scratch-def.json -d $DURATION
+#Create scratch org with a custom alias
+sfdx force:org:create -a $ALIAS -s -f config/project-scratch-def.json -d $DURATION
+
+#Set default org 
+sfdx force:config:set defaultusername=$ALIAS
+
+#Optionally install packages
+#sfdx force:package:install -p $PACKAGE_VERSION_ID
+
+#Push source to scracth org
 sfdx force:source:push
+
+#Open scracth org
 sfdx force:org:open -p /lightning/page/home
+
+#Feedback to user
 echo "Org is set up"
